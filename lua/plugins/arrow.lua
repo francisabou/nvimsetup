4d3caa7dab850a53953eca360fac7c34dadf0600
francisabou <152104510+francisabou@users.noreply.github.com>
2026-04-11 20:20:51 +0300

    Made arrow to behave like harpoon by making it open in edit mode always

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -4,14 +4,22 @@ return {
         dependencies = {
             { "echasnovski/mini.icons", lazy = true },
         },
-        opts = {
-            show_icons = true,
-            leader_key = "<C-e>",
-            save_key = "git_root",
-        },
+        config = function()
+            require("arrow").setup({
+                show_icons = false,
+                hide_handbook = true,
+                leader_key = "<C-e>",
+                save_key = "git_root",
+                window = { border = "single" },
+            })
+            -- Override arrow's leader_key mapping to open edit mode (harpoon-style)
+            vim.keymap.set("n", "<C-e>", function()
+                require("arrow.persist").open_cache_file()
+            end, { noremap = true, silent = true, nowait = true, desc = "Arrow edit list" })
+        end,
         keys = {
             { "<leader>a", function() require("arrow.persist").toggle() end, desc = "Arrow toggle file" },
-            { "<C-e>", function() require("arrow.ui").openMenu() end, desc = "Arrow menu" },
+            { "<C-e>", desc = "Arrow edit list" },
 
             { "<C-h>", function() require("arrow.persist").go_to(1) end, desc = "Arrow go to 1" },
             { "<C-j>", function() require("arrow.persist").go_to(2) end, desc = "Arrow go to 2" },
