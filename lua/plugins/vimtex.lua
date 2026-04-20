4df9dbb9084fd728d751d2627d7f70897694360c
francisabou <152104510+francisabou@users.noreply.github.com>
2026-04-20 21:57:20 +0300

    Added synctex for latexmk for bookokrat sync

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,28 +9,39 @@ return {
             vim.g.vimtex_compiler_continuous = 1
             vim.g.vimtex_view_method = "skim"
             vim.g.vimtex_compiler_latexmk = {
-                options = { "-pdf", "-interaction=nonstopmode", "-output-directory=out" },
+                options = {
+                    "-pdf",
+                    "-synctex=1",
+                    "-interaction=nonstopmode",
+                    "-output-directory=out",
+                },
             }
         end,
         -- config runs AFTER the plugin loads
         config = function()
             -- Make sure "out/" exists anytime main.tex is entered/loaded
-            vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnter" }, {
-                group = vim.api.nvim_create_augroup("vimtex-outdir", { clear = true }),
-                pattern = { "*main.tex" },
-                callback = function(args)
-                    local bufnr = args.buf
-                    local bufname = vim.api.nvim_buf_get_name(bufnr)
-                    local dir = vim.fn.fnamemodify(bufname, ":p:h")
-                    local outdir = dir .. "/out"
+            vim.api.nvim_create_autocmd(
+                { "BufRead", "BufNewFile", "BufEnter" },
+                {
+                    group = vim.api.nvim_create_augroup(
+                        "vimtex-outdir",
+                        { clear = true }
+                    ),
+                    pattern = { "*main.tex" },
+                    callback = function(args)
+                        local bufnr = args.buf
+                        local bufname = vim.api.nvim_buf_get_name(bufnr)
+                        local dir = vim.fn.fnamemodify(bufname, ":p:h")
+                        local outdir = dir .. "/out"
 
-                    if vim.fn.isdirectory(outdir) == 0 then
-                        vim.fn.mkdir(outdir, "p")
-                    end
+                        if vim.fn.isdirectory(outdir) == 0 then
+                            vim.fn.mkdir(outdir, "p")
+                        end
 
-                    vim.b[bufnr].vimtex_root = dir
-                end,
-            })
+                        vim.b[bufnr].vimtex_root = dir
+                    end,
+                }
+            )
         end,
     },
 }
